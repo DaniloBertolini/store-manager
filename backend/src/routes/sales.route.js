fe0ef1b30457e47460a3e95bd51bb72861a9dbeb
Danilo <danilocbertolini@gmail.com>
2023-11-17 14:22:40 -0300

    Limpa de arquivos

--- backend/src/routes/sales.route.js
+++ backend/src/routes/sales.route.js
@@ -4,11 +4,8 @@ const validateQuantity = require('../middlewares/validadeQuantity');
 const validateCreateSales = require('../middlewares/validateCreateSales');
 
 route.get('/', salesController.getAllSales);
-
 route.get('/:id', salesController.getSaleById);
-
 route.post('/', validateCreateSales, salesController.createSale);
-
 route.delete('/:id', salesController.deleteSale);
 
 route.put(
